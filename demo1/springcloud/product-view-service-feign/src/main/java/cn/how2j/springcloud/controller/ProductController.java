b62dfc27693f02d1dbdb355ddacfee2eccf98aae
xuezy19826 <1395219833@qq.com>
2021-05-10 19:41:25 +0800

    调通配置服务器config  改造feign失败

--- demo1/springcloud/product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
+++ demo1/springcloud/product-view-service-feign/src/main/java/cn/how2j/springcloud/controller/ProductController.java
@@ -4,6 +4,8 @@ import cn.how2j.springcloud.pojo.Product;
 import cn.how2j.springcloud.service.ProductService;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,14 @@ import java.util.List;
  * @date 2021/5/6 18:31
  */
 @Controller
+@RefreshScope
 public class ProductController {
+
+	/**
+	 * 从config-server中获取version信息
+	 */
+	@Value("${version}")
+	String version;
 
 	@Autowired
 	ProductService productService;
@@ -25,6 +34,7 @@ public class ProductController {
 	public Object products(Model m){
 		List<Product> pList = productService.listProducts();
 		m.addAttribute("ps", pList);
+		m.addAttribute("version", version);
 		return "products";
 	}
 
